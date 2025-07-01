382bba7f57db4949a124529a5fec7e3db0dd94f4
старченко дмитрий <starchenko.dmitr@mail.ru>
2025-07-01 19:05:21 +0700

    добавлена функция mask_account_card

---
+++ src/widget.py
@@ -0,0 +1,16 @@
+from mypy.types import names
+
+from masks import get_mask_card_number
+from masks import get_mask_account
+
+def mask_account_card(account: str) -> str:
+    """извлекает и строкового значения номер карты или счета и возвращает маску"""
+    words = account.split()
+    name = []
+    for word in words:
+        if word.isdigit() and len(word) == 16:
+            return f"{" ".join(name)} {get_mask_card_number(word)}"
+        elif word.isdigit() and len(word) == 20:
+            return f"{" ".join(name)} {get_mask_account(word)}"
+        else:
+            name.append(word)
