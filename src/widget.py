04cf7c207cd70fb62319803a0dec505f61df65a4
старченко дмитрий <starchenko.dmitr@mail.ru>
2025-07-01 20:29:15 +0700

    добавлена функция get_date

--- src/widget.py
+++ src/widget.py
@@ -14,3 +14,9 @@ def mask_account_card(account: str) -> str:
             return f"{" ".join(name)} {get_mask_account(word)}"
         else:
             name.append(word)
+
+
+def get_date(date: str) -> str:
+    """Декодирует дату и возврашает её в вормате ДД.ММ.ГГГГ"""
+    return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
+
