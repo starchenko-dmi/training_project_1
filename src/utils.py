61dbd2cb6e7048235ee9bb709a6778c40bfecead
старченко дмитрий <starchenko.dmitr@mail.ru>
2025-08-08 00:30:06 +0700

    Добавлен модуль external_api.py и написаны тесты на него + проверка линтерами.

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,10 @@
 import json
 
+
 def receiving_financial_transactions(json_file) -> list:
     """Функция для обработки списка транзакций из json файла"""
     try:
-        with open(json_file, 'r', encoding='utf-8') as file:
+        with open(json_file, "r", encoding="utf-8") as file:
             data = json.load(file)
             return data
     except FileNotFoundError:
