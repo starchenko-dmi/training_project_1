7b5fcb90fe1fcb9209f3e8cdef86555fa9515c7e
старченко дмитрий <starchenko.dmitr@mail.ru>
2025-08-08 01:16:56 +0700

    в функции transaction_amount добавлена конвертация по историческим данным, добавлено описание в README.md

--- src/utils.py
+++ src/utils.py
@@ -1,17 +1,17 @@
 import json
 
 
-def receiving_financial_transactions(json_file) -> list:
+def receiving_financial_transactions(file_path) -> list:
     """Функция для обработки списка транзакций из json файла"""
     try:
-        with open(json_file, "r", encoding="utf-8") as file:
+        with open(file_path, "r", encoding="utf-8") as file:
             data = json.load(file)
             return data
     except FileNotFoundError:
-        print(f"Ошибка: Файл {json_file} не найден")
+        print(f"Ошибка: Файл {file_path} не найден")
         return []
     except json.JSONDecodeError as e:
-        print(f"Ошибка: Некорректный JSON в файле {json_file}: {e}")
+        print(f"Ошибка: Некорректный JSON в файле {file_path}: {e}")
         return []
     except Exception as e:
         print(f"Неожиданная ошибка: {e}")
